bb4dc54780fca54f7f3f123c715fb17b93f0c3e8
Neil Roseman <neil@gojotter.com>
2015-12-05 08:36:08 -0800

    Initial Commit

---
+++ iTrashSorter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTrashSorter
+//
+//  Created by NEIL ROSEMAN on 12/5/15.
+//  Copyright (c) 2015 NEIL ROSEMAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
